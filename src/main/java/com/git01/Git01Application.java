521b204cc789f834d48af7b7090a11af21f6b503
nilu <ndebankura01@gmail.com>
2025-06-05 19:35:27 +0530

    first commit

---
+++ src/main/java/com/git01/Git01Application.java
@@ -0,0 +1,14 @@
+package com.git01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Git01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Git01Application.class, args);
+		System.out.println("Hello1");
+	}
+
+}
